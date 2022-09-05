5a88f2e6da222d32f7efc33ea608cd136c2f7957
Suger13 <muteki.scarlette@gmail.com>
2022-09-05 12:41:53 +0700

    feat: add pre hh

---
+++ index.js
@@ -0,0 +1,9 @@
+const myArr = ["Hello", "is", "name", "aun", "my"] //my name is aun
+const myObj = {
+    a :1
+}
+
+myArr.unshift("dddd");
+myArr.pop();
+console.log(myArr);
+console.log(typeof(typeof(myObj)))
